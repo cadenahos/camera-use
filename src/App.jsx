e31e54d0117fbacea819538e1d640b8767f4b102
cadenahos <jcadena@mantiz.pro>
2024-04-15 17:20:38 -0500

    Add basic SSL plugin to Vite config and update dependencies in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 import { Webcam } from '@webcam/react';
+import html2canvas from 'html2canvas';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const webcamRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState(null);
+
+  const capture = async () => {
+    const canvas = await html2canvas(document.querySelector("#capture"));
+    const imgData = canvas.toDataURL('image/png');
+    setImageSrc(imgData);
+    console.log('data', imgData)
+  };
 
   return (
     <>
-      <Webcam />
+      <div id="capture">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/png"
+          videoConstraints={{
+            facingMode: { exact: "environment" }
+          }}
+        />
+      </div>
+      <button onClick={capture}>Capture Photo</button>
+      {imageSrc && <img src={imageSrc} alt="captured" />}
     </>
   );
 }
